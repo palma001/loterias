a6b47feccdd9e53cbef47be5236dd3b026b6a8a9
Luis Palma <palmaluis1997@gmail.com>
2020-06-09 22:25:28 -0400

    htpps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
     }
 
     /**
