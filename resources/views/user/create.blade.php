0adde4033ef691812ff2c6174469dbcadae85b08
Luis Palma <palmaluis1997@gmail.com>
2022-04-09 02:01:04 -0400

    change

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -23,7 +23,7 @@
                         </p>
 
                         <p class="text-center selected" ng-show="hasTicket(animal.id);">
-                            <img ng-src="[[ animal.path ]]" alt="[[ animal.name ]]" width="100" height="100">
+                            <img ng-src="[[ animal.path ]]" alt="[[ animal.name ]]" style="width:100; height:100;">
                             <strong>[[ animal.number ]]</strong>
                             [[ animal.name ]]
                         </p>
