b9abc2d4635366c04239b1da6866c72d8661581c
Luis Palma <palmaluis1997@gmail.com>
2022-04-10 22:27:08 -0400

    change

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,20 +14,11 @@ class UserSeeder extends Seeder
     {
         // Administrador
         $user = new User();
-        $user->name = 'Administrador';
-        $user->username = 'admin';
-        $user->password = bcrypt('123456');
+        $user->name = 'Pespinoza';
+        $user->username = 'pespinoza';
+        $user->password = bcrypt('120189');
         $user->level = User::LEVEL_ADMIN;
         $user->print_code = '0000000';
-        $user->save();
-
-        // Taquilla inicial
-        $user = new User();
-        $user->name = 'Taquilla 1';
-        $user->username = 'taq1';
-        $user->password = bcrypt('123456');
-        $user->level = User::LEVEL_USER;
-        $user->print_code = '1111111';
         $user->save();
     }
 }
